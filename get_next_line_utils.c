5962a933b99158833dae22aefe7b9ead6de9174d
hnakai <hnakai@student.42tokyo.jp>
2023-01-05 05:01:34 +0900

    check leak

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hnakai <hnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 00:02:03 by hnakai            #+#    #+#             */
-/*   Updated: 2023/01/05 04:56:03 by hnakai           ###   ########.fr       */
+/*   Updated: 2023/01/05 05:01:25 by hnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -163,6 +163,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	ft_strlcpy(str, s1, len_s1 + 1);
 	ft_strlcat(str, s2, len_s1 + len_s2 + 1);
 	free(s1);
+	free(s2);
 	return (str);
 }
 
