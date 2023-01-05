fc13031fd89e3b82550a47c186d677fa2981a5e2
hnakai <hnakai@student.42tokyo.jp>
2023-01-05 23:18:52 +0900

    add files

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hnakai <hnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 00:02:03 by hnakai            #+#    #+#             */
-/*   Updated: 2023/01/05 23:11:35 by hnakai           ###   ########.fr       */
+/*   Updated: 2023/01/05 23:13:37 by hnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,12 +78,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	udst = (unsigned char *)dst;
 	usrc = (const unsigned char *)src;
 	i = 0;
-	printf("buff[8]:%s\n",src);
 	while (i < n)
 	{
 		udst[i] = usrc[i];
 		i++;
 	}
+	printf("buff[8]:%s\n",src);
 	return (dst);
 }
 
